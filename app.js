351721ad8773eaf2815277cdb00dd0c4d178e937
Aaron Goodrum <agoodrum@Ingram-MacBook-Pro.local>
2018-12-06 07:57:15 -0800

    add bind and arrow Function to counter

--- app.js
+++ app.js
@@ -43,20 +43,31 @@ const Player = (props) => {
 }
 
 class Counter extends React.Component {
+    state = {
+        score: 0
+    }
 
-    constructor(){
-        super()
-        this.state = {
-            score: 0
-        }
+    incrementScore(){
+        this.setState({
+            score: this.state.score + 1
+        });
+    }
+
+    decrementScore(){
+        this.setState({
+            score: this.state.score - 1
+        });
     }
 
     render(){
         return (
             <div className="counter">
-                <button className="counter-action decrement"> - </button>
+            {/* Use arrowFunction for this, or
+                Use .bind(this) to call state
+             */}
+                <button className="counter-action decrement" onClick={() => this.decrementScore()}> - </button>
                 <span className="counter-score"> {this.state.score} </span>
-                <button className="counter-action increment"> + </button>
+                <button className="counter-action increment" onClick={this.incrementScore.bind(this)}> + </button>
             </div>
         )
     }
